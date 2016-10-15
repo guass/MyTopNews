818929003ada1a24754e41bdcfcb484e91bbc219
guass <624512733@qq.com>
2016-10-15 14:57:32 +0800

    add news channel

--- app/src/main/java/com/guass/mytopnews/MainActivity.java
+++ app/src/main/java/com/guass/mytopnews/MainActivity.java
@@ -9,9 +9,11 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -19,12 +21,17 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.guass.mytopnews.activity.SearchActivity;
+import com.guass.mytopnews.bean.NewsClassify;
+import com.guass.mytopnews.tools.Constants;
 import com.guass.mytopnews.utils.BaseTools;
 import com.guass.mytopnews.view.ColumnHorizontalScrollView;
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 import com.readystatesoftware.systembartint.SystemBarTintManager;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -50,6 +57,11 @@ public class MainActivity extends AppCompatActivity {
     public ImageView shade_right;
     private EditText et_searchview;
     private SystemBarTintManager tintManager;
+    /** 新闻分类列表*/
+    private ArrayList<NewsClassify> newsClassify=new ArrayList<NewsClassify>();
+    /**当前选择的栏目*/
+   int  columnSelectIndex = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -111,6 +123,79 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this,SearchActivity.class));
             }
         });
+
+        setChangelView();
+    }
+
+    /**
+     *  当栏目项发生变化时候调用
+     * */
+    private void setChangelView() {
+        initColumnData();
+        initTabColumn();
+        initFragment();
+    }
+
+    private void initFragment()
+    {
+    }
+
+    /** 获取Column栏目 数据*/
+    private void initColumnData() {
+        newsClassify = Constants.getData();
+    }
+
+    private void initTabColumn()
+    {
+        mRadioGroup_content.removeAllViews();
+        int count = newsClassify.size();
+
+        mColumnHorizontalScrollView.setParam(this, mScreenWidth, mRadioGroup_content, shade_left, shade_right, ll_more_columns, rl_column);
+        for(int i = 0; i < count; i++)
+        {
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(mItemWidth, ViewGroup.LayoutParams.WRAP_CONTENT);
+            params.leftMargin = 5;
+            params.rightMargin = 5;
+            //			TextView localTextView = (TextView) mInflater.inflate(R.layout.column_radio_item, null);
+            TextView columnTextView = new TextView(this);
+            columnTextView.setTextAppearance(this, R.style.top_category_scroll_view_item_text);
+//			localTextView.setBackground(getResources().getDrawable(R.drawable.top_category_scroll_text_view_bg));
+            columnTextView.setBackgroundResource(R.drawable.radio_buttong_bg);
+            columnTextView.setGravity(Gravity.CENTER);
+            columnTextView.setPadding(5, 5, 5, 5);
+            columnTextView.setId(i);
+            columnTextView.setText(newsClassify.get(i).getTitle());
+            columnTextView.setTextColor(getResources().getColorStateList(R.color.top_category_scroll_text_color_day));
+            if(columnSelectIndex == i)
+            {
+                columnTextView.setSelected(true);
+            }
+
+            columnTextView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v)
+                {
+                    for(int i = 0; i < mRadioGroup_content.getChildCount(); i++)
+                    {
+                        View localView = mRadioGroup_content.getChildAt(i);
+                        if(localView != v)
+                        {
+                            Log.i(TAG, "onClick: 1");
+                            localView.setSelected(false);
+                        }
+                        else
+                        {
+                            Log.i(TAG, "onClick: 2");
+                            localView.setSelected(true);
+                            mViewPager.setCurrentItem(i);
+                        }
+                    }
+                }
+            });
+            mRadioGroup_content.addView(columnTextView,i,params);
+        }
+
+
     }
 
 }
